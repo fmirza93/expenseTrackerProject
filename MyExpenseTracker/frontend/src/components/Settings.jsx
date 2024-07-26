a19f0bb7befbbe58d40b46c3df6e379ce5d84ecc
Stephen Vincent <stevevincent17@icloud.com>
2024-07-26 10:28:45 +0100

    written TODO's in the settings component.

--- MyExpenseTracker/frontend/src/components/Settings.jsx
+++ MyExpenseTracker/frontend/src/components/Settings.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div>
+      <h1>Settings Page</h1>
+
+      {/* Settings form to go here. There is a button component that can be used for the buttons that has a variant property. */}
+
+      {/* The user should be able to:
+      1. Change their photo
+      2. Update their name
+      3. Set their monthly income
+      4. Set their currency
+      5. Save their preferences
+      */}
+
+
+    </div>
   )
 }
 
