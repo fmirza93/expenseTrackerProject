49fef7852710a8ae29907faa47c5fdd7d0d168ae
Stephen Vincent <stevevincent17@icloud.com>
2024-07-26 10:41:28 +0100

    adjust folder and file structure

--- MyExpenseTracker/frontend/src/components/Settings.jsx
+++ MyExpenseTracker/frontend/src/components/Settings.jsx
@@ -2,21 +2,7 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings Page</h1>
-
-      {/* Settings form to go here. There is a button component that can be used for the buttons that has a variant property. */}
-
-      {/* The user should be able to:
-      1. Change their photo
-      2. Update their name
-      3. Set their monthly income
-      4. Set their currency
-      5. Save their preferences
-      */}
-
-
-    </div>
+    <div>Settings</div>
   )
 }
 
