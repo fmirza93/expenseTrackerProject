71a684fd21c717ae5a030f2d66efdebb832ea00f
Stephen Vincent <stevevincent17@icloud.com>
2024-08-04 16:05:11 +0100

    adjusted the readme, addExpense and SetAllowance components

--- frontend/src/components/AddExpense.jsx
+++ frontend/src/components/AddExpense.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CategoryDropdown from './CategoryDropdown'
-import Button from "../components/Button"
+import Button from "./Button"
 
 const AddExpense = () => {
   return (
