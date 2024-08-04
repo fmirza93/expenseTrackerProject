432e435e29c070abf013387a60448c3ca90821b3
Stephen Vincent <stevevincent17@icloud.com>
2024-08-04 16:25:21 +0100

    Updated the folder structure

--- frontend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-  moduleFileExtensions: ["js", "jsx", "json", "node"],
-  setupFilesAfterEnv: ["./jest.setup.js"],
-  testMatch: ["**/src/tests/**/*.[jt]s?(x)"],
-};
